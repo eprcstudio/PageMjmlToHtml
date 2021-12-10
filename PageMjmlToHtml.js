b791bf047eceeedea7fced562e1ef26f086905fa
Romain Cazier <tools@romaincazier.com>
2021-12-10 11:15:22 +0100

    Switch to Promise-based (and escape early if not supported)

--- PageMjmlToHtml.js
+++ PageMjmlToHtml.js
@@ -1,75 +1,90 @@
-window.addEventListener("load", () => {
+window.addEventListener("load", function() {
+	if(typeof Promise !== "function") return;
 	const copyBtn = document.querySelector(".mjml2html_btn");
 	if (copyBtn) {
-		let timeout;
 		copyBtn.addEventListener("click", (e) => {
 			e.preventDefault();
 			const copyBtnLabel = copyBtn.innerHTML;
 			copyBtn.innerHTML = copyBtn.dataset.copying;
 			copyBtn.style.pointerEvents = "none";
-			loadPage(
-				copyBtn.href,
-				(content) => {
-					copyTextToClipboard(content);
-					copyBtn.innerHTML = copyBtn.dataset.copied;
-					timeout = setTimeout(() => {
-						copyBtn.innerHTML = copyBtnLabel;
-						copyBtn.style.pointerEvents = "";
-					}, 2000);
-				},
-				() => {
+			loadPage(copyBtn.href)
+				.then((content) => {
+					copyTextToClipboard(content)
+						.then(() => copyBtn.innerHTML = copyBtn.dataset.copied)
+						.catch(() => copyBtn.innerHTML = copyBtn.dataset.error)
+						.finally(() => {
+							setTimeout(() => {
+								copyBtn.innerHTML = copyBtnLabel;
+								copyBtn.style.pointerEvents = "";
+							}, 2000);
+						});
+				})
+				.catch(() => {
 					copyBtn.innerHTML = copyBtnLabel;
 					copyBtn.style.pointerEvents = "";
-				}
-			);
+				});
 		});
 	}
-});
-function loadPage(url, callback, error) {
-	const xhr = new XMLHttpRequest();
-	xhr.addEventListener('readystatechange', (e) => {
-		if (xhr.readyState === XMLHttpRequest.DONE) {
-			if (xhr.status === 0 || (xhr.status >= 200 && xhr.status < 400)) {
-				if (xhr.responseType === "json") {
-					xhr.responseText = JSON.parse(xhr.responseText);
+	function loadPage(url) {
+		return new Promise((resolve, reject) => {
+			const xhr = new XMLHttpRequest();
+			xhr.addEventListener('readystatechange', (e) => {
+				if (xhr.readyState === XMLHttpRequest.DONE) {
+					if (xhr.status === 0 || (xhr.status >= 200 && xhr.status < 400)) {
+						if (xhr.responseType === "json") {
+							xhr.responseText = JSON.parse(xhr.responseText);
+						}
+						resolve(xhr.responseText);
+					} else {
+						reject();
+					}
 				}
-				callback(xhr.responseText);
+			});
+			xhr.open('GET', url, true);
+			xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+			xhr.send();
+		});
+	}
+	// https://stackoverflow.com/a/30810322
+	function copyTextToClipboard(text) {
+		return new Promise((resolve, reject) => {
+			if (!navigator.clipboard) {
+				if (fallbackCopyTextToClipboard(text)) {
+					resolve();
+				} else {
+					reject();
+				}
 			} else {
-				error();
+				navigator.clipboard.writeText(text)
+					.then(() => resolve())
+					.catch((err) => {
+						console.error("Could not copy text: ", err);
+						reject();
+					});
 			}
+		});
+	}
+	function fallbackCopyTextToClipboard(text) {
+		const textArea = document.createElement("textarea");
+		textArea.value = text;
+		textArea.style.top = "0";
+		textArea.style.left = "0";
+		textArea.style.position = "fixed";
+		document.body.appendChild(textArea);
+		textArea.focus();
+		textArea.select();
+		let success = true;
+		try {
+			var command = document.execCommand("copy");
+			if (!command) {
+				console.log("An error occured using the copying text command");
+				success = false;
+			}
+		} catch (err) {
+			console.error("Could not copy text: ", err);
+			success = false;
 		}
-	});
-	xhr.open('GET', url, true);
-	xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-	xhr.send();
-}
-// https://stackoverflow.com/a/30810322
-function fallbackCopyTextToClipboard(text) {
-	const textArea = document.createElement("textarea");
-	textArea.value = text;
-	textArea.style.top = "0";
-	textArea.style.left = "0";
-	textArea.style.position = "fixed";
-	document.body.appendChild(textArea);
-	textArea.focus();
-	textArea.select();
-	try {
-		var successful = document.execCommand('copy');
-		var msg = successful ? 'successful' : 'unsuccessful';
-		console.log('Fallback: Copying text command was ' + msg);
-	} catch (err) {
-		console.error('Fallback: Oops, unable to copy', err);
+		document.body.removeChild(textArea);
+		return success;
 	}
-	document.body.removeChild(textArea);
-}
-function copyTextToClipboard(text) {
-	if (!navigator.clipboard) {
-		fallbackCopyTextToClipboard(text);
-		return;
-	}
-	navigator.clipboard.writeText(text).then(function() {
-		console.log('Async: Copying to clipboard was successful!');
-	}, function(err) {
-		console.error('Async: Could not copy text: ', err);
-	});
-}
+});
